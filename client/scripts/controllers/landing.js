6a72176c6643bcfb3e375708158e48e019a394e1
Scott Jason <scottleejason@gmail.com>
2015-10-13 21:44:05 -0700

    inits recommendations

--- client/scripts/controllers/landing.js
+++ client/scripts/controllers/landing.js
@@ -22,14 +22,35 @@ function Landing($scope, $state, $timeout, RequestApi, $firebaseObject, $firebas
   var init = function() {
     var likedItems = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/liked-items');
     var dislikedItems = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/disliked-items');
+    var fittingRecommendations = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/recommendations');
     var fittinRoom = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/products');
     var customers = new Firebase('https://retail-store-app.firebaseio.com/customers');
     likedItems.remove();
     dislikedItems.remove();
     fittinRoom.remove();
     customers.remove();
+    fittingRecommendations.remove();
     var products = new Firebase('https://retail-store-app.firebaseio.com/products');
     $scope.products = $firebaseArray(products);
+
+    fittingRecommendations.on("child_added", function(snapshot) {
+        $timeout(function() {
+          var product = snapshot.val();
+          $scope.productSelected.title = product.title;
+          $scope.productSelected.photo = product.photo;
+          $scope.productSelected.firstShown = {};
+          $scope.productSelected.firstShown.title = product.firstShown.title;
+          $scope.productSelected.isRecommendItem = true;
+          $scope.showLoader = false;
+          $scope.showRecommendation = true;
+          $scope.showTryOn = true;
+          console.log('incoming recommendation', snapshot.val());
+        });
+
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
   };
 
   init();
@@ -56,41 +77,110 @@ function Landing($scope, $state, $timeout, RequestApi, $firebaseObject, $firebas
   };
 
   $scope.likeItem = function(product) {
-    $scope.productSelected.title = product.title
-    var likedItems = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/liked-items');
-    $scope.productSelected.customerId = $scope.customer.id;
-    $scope.productSelected.customerName = $scope.customer.name;
-    delete $scope.productSelected.$id;
-    delete $scope.productSelected.$priority;
-    likedItems.push($scope.productSelected);
-    $scope.showThankYou = true;
-  };
+    console.log('like item called');
+    var isRecommendItem = product.isRecommendItem;
+    if (!isRecommendItem) {
+      $scope.productSelected.title = product.title
+      var likedItems = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/liked-items');
+      $scope.productSelected.customerId = $scope.customer.id;
+      $scope.productSelected.customerName = $scope.customer.name;
+      delete $scope.productSelected.$id;
+      delete $scope.productSelected.$priority;
+      likedItems.push($scope.productSelected);
+      $scope.showThankYou = true;
+    } else {
+      console.log('is a liked recommened item');
 
-  $scope.dislikeItem = function(product) {
+      var foundMatch = null;
+      var recommendations = new Firebase('https://retail-store-app.firebaseio.com/recommendations');
+      recommendations.on('value', function(snapshot) {
+
+          var recommendations = snapshot.val();
+
+          var firstShownName = product.firstShown.title; // first shown product
+          var recommendedName = product.title; // recommeneded product
+          var count = 0;
+          if (recommendations) {
+            angular.forEach(recommendations, function(val, key) {
+                if (key === 'firstShownName') {
+                  var isMatch = (val === firstShownName);
+                  if (isMatch) {
+                    count++
+                  }
+                }
+                if (key === 'recommendedName') {
+                  var isMatch = (val === recommendedName);
+                  if (isMatch) {
+                    count++
+                  }
+
+                }
+                if (count === 2) {
+                  console.log('found match');
+                  
+                }
+              } else if (index === 1 && !!count) {
+                var isMatch = (key === recommendedName);
+                if (isMatch) {
+                  foundMatch = true;
+                  console.log('found matching association');
+                  recommendation.score++
+                    var recommendations = new Firebase('https://retail-store-app.firebaseio.com/recommendations');
+                  recommendations.set(recommendation);
+                }
+              }
+            });
+
+          console.log('end iteration')
+        }
+
+      });
+  }
+
+};
+
+//   // after the iteration is complete, check if a match was found
+// if (!foundMatch) {
+//   // if not create and save the new association
+//   console.log('no match found');
+//   var recommendations = new Firebase('https://retail-store-app.firebaseio.com/recommendations');
+//   var obj = {};
+//   obj.firstShownName = firstShownName;
+//   obj.recommendedName = recommendedName;
+//   obj.score = 1;
+//   recommendations.push(obj);
+// }
+
+$scope.dislikeItem = function(product) {
+  var isRecommendItem = product.isRecommendItem;
+  if (!isRecommendItem) {
     var dislikedItems = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/disliked-items');
     product.customerId = $scope.customer.id;
     product.customerName = $scope.customer.name;
     delete product.$id;
     delete product.$priority;
     dislikedItems.push(product);
-    $scope.showThankYou = true;
-  };
+    $scope.showLoader = true;
+  } else {
+    console.log('is a disliked recommened item');
+  }
+};
 
-  $scope.getNewSize = function(product) {
-    var newSizeItems = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/newsize-items');
-    product.customerId = $scope.customer.id;
-    product.customerName = $scope.customer.name;
-    delete product.$id;
-    delete product.$priority;
-    newSizeItems.push(product);
-    $scope.showSizeRequested = true;
-  };
+$scope.getNewSize = function(product) {
+  var newSizeItems = new Firebase('https://retail-store-app.firebaseio.com/fitting-room/newsize-items');
+  product.customerId = $scope.customer.id;
+  product.customerName = $scope.customer.name;
+  delete product.$id;
+  delete product.$priority;
+  newSizeItems.push(product);
+  $scope.showSizeRequested = true;
+};
 
-  $scope.goAgain = function() {
-    $state.go($state.current, {}, {
-      reload: true
-    });
-  };
+$scope.goAgain = function() {
+  $state.go($state.current, {}, {
+    reload: true
+  });
+};
 
-  Landing.$inject['$scope', '$state', '$timeout', 'RequestApi', '$firebaseObject', '$firebaseArray'];
+Landing.$inject['$scope', '$state', '$timeout', 'RequestApi', '$firebaseObject', '$firebaseArray'];
 }
